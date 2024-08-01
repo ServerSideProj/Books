eed4e3372f00c0bd3a1822ffbc066ccee4b63569
hilatsivion <hilatsivion2@gmail.com>
2024-08-01 13:06:14 +0300

    add authors to home page

--- frontend/components/SmallCard/smcard.js
+++ frontend/components/SmallCard/smcard.js
@@ -3,17 +3,18 @@ $(document).ready(function () {
 });
 
 const generatecards = () => {
-  for (let j = 0; j < 3; j++) {
-    for (let i = 0; i < 10; i++) {
+  for (let j = 1; j <= 2; j++) {
+    for (let i = 0; i < 8; i++) {
       const card = `
         <div class="sm-card">
             <img src="" alt="" class="image-author" />
             <div class="container-flex-col">
             <p class="xl-text">name of author</p>
-            <p class="sm-text grey-text">name of author</p>
+            <p class="sm-text grey-text">Number of books: <span class="books-amount">4</span></p>
             </div>
         </div>`;
-      $(`.line${j}`).append(card);
+      $(`.line${j}-a`).append(card);
+      $(`.line${j}-b`).append(card);
     }
   }
 };

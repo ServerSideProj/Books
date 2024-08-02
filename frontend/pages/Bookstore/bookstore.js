55989d176fdb6e7cb80efe382cc1421fb14648b8
hilatsivion <hilatsivion2@gmail.com>
2024-08-02 14:15:34 +0300

    top-filters-nav bookstore

---
+++ frontend/pages/Bookstore/bookstore.js
@@ -0,0 +1,36 @@
+const isLoggedIn = localStorage.getItem("authToken") !== null;
+var arrBooks = []; // the books that available on screen
+$(document).ready(function () {
+  // Initial render of books
+  renderBooks(arrBooks);
+
+  $(".price-filter").on("click", function () {
+    $(".price-filter > img").toggleClass("open");
+    $(".sm-popup").toggleClass("open");
+  });
+
+  // Event handler for filter price
+  $(".sm-popup p").on("click", function () {
+    $(".sm-popup p").removeClass("selected");
+    $(this).addClass("selected");
+    $(".price-filter p").html(this.outerText);
+
+    const filter = $(this).data("price-filter");
+    sortAndRenderBooks(filter);
+  });
+});
+
+// get all books from server
+const getAllBooks = () => {
+  // arrBooks =
+};
+
+// Render books on screen
+const renderBooks = (books) => {
+  const bookstore = $(".books-container");
+  bookstore.empty();
+
+  books.forEach((book) => {
+    bookstore.append(generateBookCard(false));
+  });
+};

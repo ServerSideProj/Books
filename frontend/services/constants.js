681910eeb651f471e52e8feed8da9a68f7bdaa96
Ran Meshulam <ranmes16@gmail.com>
2024-08-22 01:09:49 +0300

    x

--- frontend/services/constants.js
+++ frontend/services/constants.js
@@ -1,15 +1,13 @@
 /////// לשנות את כל הערכים לפי הנתיבים הנכונים
 
 // For home:
-const PORT = "7136";
-const API_URL = `https://localhost:${PORT}/api/`;
+// const PORT = "7136";
+// const API_URL = `https://localhost:${PORT}/api/`;
 
-const FILE_URL = `https://localhost:${PORT}/api/FileUpload`;
-const IMAGE_URL = `https://localhost:${PORT}/Images/`;
+// const FILE_URL = `https://localhost:${PORT}/api/FileUpload`;
+// const IMAGE_URL = `https://localhost:${PORT}/Images/`;
 
 // For Ruppin server
-// export const COURSES_URL = `https://proj.ruppin.ac.il/cgroup80/test2/tar1/api/courses`;
-// export const INSTRUCTORS_URL = `https://proj.ruppin.ac.il/cgroup80/test2/tar1/api/instructors`;
-// export const USERS_URL = `https://proj.ruppin.ac.il/cgroup80/test2/tar1/api/Users`;
-// export const FILE_URL = `https://proj.ruppin.ac.il/cgroup80/test2/tar1/api/FileUpload`;
-// export const IMAGE_URL = `https://proj.ruppin.ac.il/cgroup80/test2/tar1/Images`;
+const API_URL = `https://proj.ruppin.ac.il/cgroup80/test2/tar1/api/`;
+const FILE_URL = `https://proj.ruppin.ac.il/cgroup80/test2/tar1/api/FileUpload/`;
+const IMAGE_URL = `https://proj.ruppin.ac.il/cgroup80/test2/tar1/Images/`;
